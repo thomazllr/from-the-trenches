e430881333a5e8d8497dd856bea6e7076cb3d267
thomazllr <guithomaz270@gmail.com>
2025-08-03 02:51:23 -0300

    feat: implementando servico de users

---
+++ user-service/src/main/java/com/thomazllr/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.thomazllr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeServiceApplication.class, args);
+
+    }
+
+
+}
---
+++ user-service/src/test/java/com/thomazllr/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomazllr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
